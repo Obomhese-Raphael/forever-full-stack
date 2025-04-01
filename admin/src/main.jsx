60feadf963a10d5994776d2f66e282abc89d8315
ObomheseR <obomheser@gmail.com>
2025-04-01 02:16:56 +0100

    Enhance admin panel configuration and update dependencies

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,16 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
